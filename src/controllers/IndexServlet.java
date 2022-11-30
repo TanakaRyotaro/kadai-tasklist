87536acc1733f3fe4ca3b9aa27aa3a6c1f99d92f
Tanaka Ryotaro <c-rytanaka@yahoo-corp.jp>
2022-11-30 13:05:08 +0900

    getAllMessagesを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,31 +1,32 @@
-package controllers;
+package controllers; //データベースから複数のメッセージ情報を取得して一覧表示する
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class IndexServlet
- */
+import models.Message;
+import utils.DBUtil;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public IndexServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    EntityManager em = DBUtil.createEntityManager();
 
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        em.close();
 	}
-
 }
